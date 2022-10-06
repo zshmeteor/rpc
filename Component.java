da31c38a21394a5031767d742ddbdc74095fadf6
zshmeteor <90160331+zshmeteor@users.noreply.github.com>
2022-10-06 14:25:49 +0800

    Create Component.java

---
+++ Component.java
@@ -0,0 +1,13 @@
+package glimmer.zsh.rpc;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+@Target({TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Component {
+
+}
